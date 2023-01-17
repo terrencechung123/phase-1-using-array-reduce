efb221b2b5e015f7f1df9574b4e7d88e0295c004
Terrence Chung <terrencechung35@gmail.com>
2023-01-17 14:33:19 -0800

    lab

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+/*let totalBatteries = " ";*/
+
+const totalBatteries = batteryBatches.reduce(add, 0);
+function add(accumulator, a){
+    return accumulator + a;
+}
